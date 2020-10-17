8ac018b870a4c9d8cacc3b35a5c621c77ff6af52
Ida <kurealkatraz@gmail.com>
2020-10-17 17:33:35 +0200

    fix: btn shadow on card hover

--- components/building/cards/preview.vue
+++ components/building/cards/preview.vue
@@ -57,6 +57,10 @@
 		align-items: center;
 		width: calc(100% - 80px);
 	}
+
+	.btnWrapper {
+		filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+	}
 </style>
 <style scoped>
 	@media screen and (min-width: 600px) {
